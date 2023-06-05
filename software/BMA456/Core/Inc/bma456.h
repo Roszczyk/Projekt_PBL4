c4dd81b3094cb3a084fdce9fa62683f35fb93a85
nkrzykal <01168987@pw.edu.pl>
2023-06-05 12:01:19 +0200

    bma 456 kod oddzielny

---
+++ software/BMA456/Core/Inc/bma456.h
@@ -0,0 +1,22 @@
+/*
+ * bma456.h
+ *
+ *  Created on: 29 maj 2023
+ *      Author: natha
+ */
+
+#ifndef INC_BMA456_H_
+#define INC_BMA456_H_
+
+#define BMA4456_OK         0
+#define BMA456_CHIPID      0x00
+#define BMA456_CMD         0x7E
+#define BMA456_PWR_CTRL    0x7D
+#define BMA456_PWR_CONF    0x7C
+#define BMA456_INIT_CTRL   0x59
+#define BMA456_ACC_CONF    0x40
+
+#define BMA456_FEATURES_IN 0x5E
+#define FEATURES_LEN       12
+
+#endif /* INC_BMA456_H_ */
