82cc60905841bb0b27e33028654c893e7ce6406b
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-06-07 12:38:56 +0200

    Wstępna obróbka danych z akcelerometru#2

--- software/signals.py
+++ software/signals.py
@@ -11,7 +11,7 @@ tensometer=[]
 for i in range(100):
     time.append(i)
     tensometer.append(np.random.choice([0,1])*50)
-    temperature.append(np.random.choice([21,22,23,24,25,26,27,28]))
+    temperature.append(np.random.choice([21,22,23,24,25,26,27,28])*2)
     accelerometer.append(np.random.choice([0,1])*50)
     rssi.append(np.random.choice([-63,-64,-65,-66,-67])+100)
     moisture.append(np.random.choice([40,50,60,70,45,55,65]))
