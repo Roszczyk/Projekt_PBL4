f54fee6ad7915a8e9b6f4c672861fd876ef56a38
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-06-14 16:17:21 +0200

    Skrypt Python do analizy zebranych danych

--- software/signals.py
+++ software/signals.py
@@ -1,25 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sys
 
-time=[]
-temperature=[]
-accelerometer=[]
-moisture=[]
-rssi=[]
-tensometer=[]
+def initFile(fileName): #odczytanie danych z pliku i stworzenie obiektów danych
+    path_to_data=os.path.join(sys.path[0], fileName)
+    file=open(path_to_data)
+    dataArray=[]
+    temperature=[]
+    accelerometer=[]
+    moisture=[]
+    tensometer=[]
+    time=[]
+    timeValue=0
+    file_text=file.read()
+    file_lines=file_text.split("\n")
+    for i in range(len(file_lines)-1):
+        file_line=file_lines[i]
+        line_array=file_line.split(" ")
+        line_array.pop(0)
+        if line_array[0]=="Temperatura:":
+            temperature.append((float(line_array[1])-25)*8)
+        if line_array[0]=="Wilgotnosc:":
+            moisture.append(float(line_array[1]))
+        if line_array[0]=="Grawitacja:":
+            # print(line_array)
+            if line_array[1]=="-z":
+                accelerometer.append(50)
+            else:
+                accelerometer.append(0)
+        if line_array[0]=="Lezy":
+            print("chuj")
+            tensometer.append(50)
+        if line_array[0]=="Nie" and line_array[1]=="lezy":
+            tensometer.append(0)
+        if line_array[0]=="Wstaje":
+            timeValue=timeValue+1
+            time.append(timeValue)
+            if len(accelerometer)<(len(time)-1):
+                accelerometer.append(0)
+            if len(tensometer)<(len(time)-1):
+                tensometer.append(0)
+        
+        
+    file.close()
+    return temperature, moisture, accelerometer, tensometer, time
 
-for i in range(100):
-    time.append(i)
-    tensometer.append(np.random.choice([0,1])*50)
-    temperature.append(np.random.choice([21,22,23,24,25,26,27,28])*2)
-    accelerometer.append(np.random.choice([0,1])*50)
-    rssi.append(np.random.choice([-63,-64,-65,-66,-67])+100)
-    moisture.append(np.random.choice([40,50,60,70,45,55,65]))
+temperature, moisture, accelerometer, tensometer, time=initFile("data1.txt")
+
+print(len(temperature), len(moisture), len(accelerometer), len(tensometer), len(time))
+
+# print(temperature, moisture, accelerometer, tensometer, time)
 
 plt.plot(time,temperature, '.-')
 plt.plot(time,accelerometer, '-')
 plt.plot(time,moisture, '.-')
-plt.plot(time,rssi, '.-')
 plt.plot(time,tensometer, '-')
 plt.grid()
 plt.show()
